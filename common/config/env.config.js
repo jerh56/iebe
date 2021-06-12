72d9023e8ab2d4aa1da4fc49368f0314e0b32e0f
Juan Ernesto Ramos Hernandez <eramos@coppel.com>
2021-06-11 22:25:45 -0600

    update env.config.js

--- common/config/env.config.js
+++ common/config/env.config.js
@@ -1,7 +1,9 @@
+const http = require('http');
+let port = process.env.PORT;
 module.exports = {
-    "port": 3600,
-    "appEndpoint": "http://localhost:3600",
-    "apiEndpoint": "http://localhost:3600",
+    "port": port ,
+    "appEndpoint": "http://localhost:`port`",
+    "apiEndpoint": "http://localhost:`port`",
     "jwt_secret": "myS33!!creeeT",
     "jwt_expiration_in_seconds": 36000,
     "environment": "dev",
