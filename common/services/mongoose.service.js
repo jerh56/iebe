73aea672e6cc50516a7b9b6a6944355ddae72632
Juan Ernesto Ramos Hernandez <eramos@coppel.com>
2021-07-10 16:59:00 -0600

    create secrets, add error handling

--- common/services/mongoose.service.js
+++ common/services/mongoose.service.js
@@ -1,26 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
 let count = 0;
 
 const options = {
-    autoIndex: false, // Don't build indexes
-    poolSize: 10, // Maintain up to 10 socket connections
-    // If not connected, return errors immediately rather than waiting for reconnect
-    bufferMaxEntries: 0,
-    // all other approaches are now deprecated by MongoDB:
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-    
+  autoIndex: app.get("env") != "production", // Don't build indexes
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0,
+  // all other approaches are now deprecated by MongoDB:
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
 const connectWithRetry = () => {
-    console.log('MongoDB connection with retry')
-    mongoose.connect("mongodb+srv://admin:1j79ol4f@cluster0.8fpoz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", options).then(()=>{
-        console.log('MongoDB is connected')
-    }).catch(err=>{
-        console.log('MongoDB connection unsuccessful, retry after 5 seconds. ', ++count);
-        setTimeout(connectWithRetry, 5000)
+  console.log("MongoDB connection with retry");
+  const connstring = process.env.CONN_STRING;
+  console.log(connstring);
+  // connection string example: "user:123456@cluster0.8fpoz.mongodb.net/iedb-dev"
+  // Windows: set CONN_STRING=user:123456@cluster0.8fpoz.mongodb.net/iedb-dev
+  // Linux: export CONN_STRING=user:123456@cluster0.8fpoz.mongodb.net/iedb-dev
+  mongoose
+    .connect(`mongodb+srv://${connstring}?retryWrites=true&w=majority`, options)
+    .then(() => {
+      console.log("MongoDB is connected");
     })
+    .catch((err) => {
+      console.log(
+        "MongoDB connection unsuccessful, retry after 5 seconds. ",
+        ++count
+      );
+      setTimeout(connectWithRetry, 5000);
+    });
 
-    /*mongoose.connect("mongodb://localhost:27017/rest-tutorial", options).then(()=>{
+  /*mongoose.connect("mongodb://localhost:27017/rest-tutorial", options).then(()=>{
         console.log('MongoDB is connected')
     }).catch(err=>{
         console.log('MongoDB connection unsuccessful, retry after 5 seconds. ', ++count);
--- users/controllers/users.controller.js
+++ users/controllers/users.controller.js
@@ -1,62 +1,67 @@
-const UserModel = require('../models/users.model');
-const crypto = require('crypto');
+const UserModel = require("../models/users.model");
+const crypto = require("crypto");
 
 exports.insert = (req, res) => {
-    let salt = crypto.randomBytes(16).toString('base64');
-    let hash = crypto.createHmac('sha512', salt).update(req.body.password).digest("base64");
-    req.body.password = salt + "$" + hash;
-    req.body.permissionLevel = 1;
-    UserModel.createUser(req.body)
-        .then((result) => {
-            res.status(201).send({id: result._id});
-        });
+  let salt = crypto.randomBytes(16).toString("base64");
+  let hash = crypto
+    .createHmac("sha512", salt)
+    .update(req.body.password)
+    .digest("base64");
+  req.body.password = salt + "$" + hash;
+  req.body.permissionLevel = 1;
+  UserModel.createUser(req.body)
+    .then((result) => {
+      res.status(201).send({ id: result._id });
+    })
+    .catch((err) =>
+      res.status(409).send({ error: `Error: ${err} - usuario ya registrado` })
+    );
 };
 
 exports.list = (req, res) => {
-    let limit = req.query.limit && req.query.limit <= 100 ? parseInt(req.query.limit) : 10;
-    let page = 0;
-    if (req.query) {
-        if (req.query.page) {
-            req.query.page = parseInt(req.query.page);
-            page = Number.isInteger(req.query.page) ? req.query.page : 0;
-        }
+  console.log(re.query);
+  let limit =
+    req.query.limit && req.query.limit <= 100 ? parseInt(req.query.limit) : 10;
+  let page = 0;
+  if (req.query) {
+    if (req.query.page) {
+      req.query.page = parseInt(req.query.page);
+      page = Number.isInteger(req.query.page) ? req.query.page : 0;
     }
-    UserModel.list(limit, page)
-        .then((result) => {
-            res.status(200).send(result);
-        })
+  }
+  UserModel.list(limit, page).then((result) => {
+    res.status(200).send(result);
+  });
 };
 
 exports.search = (req, res) => {
-    UserModel.findBySkill(req.query['skill'])
-        .then((result) => {
-            res.status(200).send(result);
-        });
+  UserModel.findBySkill(req.query["skill"]).then((result) => {
+    res.status(200).send(result);
+  });
 };
 
 exports.getById = (req, res) => {
-    UserModel.findById(req.params.userId)
-        .then((result) => {
-            res.status(200).send(result);
-        });
+  UserModel.findById(req.params.userId).then((result) => {
+    res.status(200).send(result);
+  });
 };
 exports.patchById = (req, res) => {
-    if (req.body.password) {
-        let salt = crypto.randomBytes(16).toString('base64');
-        let hash = crypto.createHmac('sha512', salt).update(req.body.password).digest("base64");
-        req.body.password = salt + "$" + hash;
-    }
+  if (req.body.password) {
+    let salt = crypto.randomBytes(16).toString("base64");
+    let hash = crypto
+      .createHmac("sha512", salt)
+      .update(req.body.password)
+      .digest("base64");
+    req.body.password = salt + "$" + hash;
+  }
 
-    UserModel.patchUser(req.params.userId, req.body)
-        .then((result) => {
-            res.status(204).send({});
-        });
-
+  UserModel.patchUser(req.params.userId, req.body).then((result) => {
+    res.status(204).send({});
+  });
 };
 
 exports.removeById = (req, res) => {
-    UserModel.removeById(req.params.userId)
-        .then((result)=>{
-            res.status(204).send({});
-        });
+  UserModel.removeById(req.params.userId).then((result) => {
+    res.status(204).send({});
+  });
 };
